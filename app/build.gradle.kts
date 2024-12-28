e37a1db1a9fb9db38183fc67b5670f45b76a7d8b
Tatianajimenez12 <tatiana_jr@tesch.edu.mx>
2024-12-27 18:54:22 -0600

    Hice varios cambios asi como la implementacion de firebase en android studio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.gestor_capsa"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.gestor_capsa"
         minSdk = 33
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
